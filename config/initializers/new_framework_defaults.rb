fe9aba92955299b514d99fec2a13b37ae06e71d1
Franklin Yu <franklinyu@hotmail.com>
2016-09-05 12:21:02 -0400

    Include only the corresponding helper
    Reset to classic behavior, hopefully improve performance by decreasing
    load time.

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -16,3 +16,11 @@ ActiveSupport.halt_callback_chains_on_return_false = false
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+
+# If false, `FooController` only includes module FooHelper from `app/helpers/foo_helper.rb`,
+# instead of all the helpers in `app/helpers/` directory.
+# `BarHelper` can be manually included in `FooController` with the help of [`helper` method][1].
+# Previous versions had false.
+#
+# [1]: http://api.rubyonrails.org/v5.0.0.1/classes/AbstractController/Helpers/ClassMethods.html#method-i-helper
+Rails.application.config.action_controller.include_all_helpers = false
