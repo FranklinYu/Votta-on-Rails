d0601ad091aea638e97a7d717d1694a001d48030
Franklin Yu <franklinyu@hotmail.com>
2016-09-06 16:03:57 -0400

    Generate Session class
    Generated with
    
        rails generate model Session user:references comment:string
        rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904065701) do
+ActiveRecord::Schema.define(version: 20160906193429) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
