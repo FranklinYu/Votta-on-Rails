b2e18a639224ca2d326475b0b36c1d2b70332473
Franklin Yu <franklinyu@hotmail.com>
2016-09-06 18:23:26 -0400

    Associate one User with many Sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :sessions, dependent: :destroy
+
   validates :email, presence: true, uniqueness: true
   validates :password, length: {minimum: 8}
 end
