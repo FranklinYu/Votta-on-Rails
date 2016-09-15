310c00b3cb0287b5f7ed588e5f7e34b92ad90c29
Franklin Yu <franklinyu@hotmail.com>
2016-09-15 14:42:33 -0400

    Refine matchers in RSpec tests
    Thanks to [`parsed_body`][1], [`with_indifferent_access`][2] and [RSpec
    composing matchers][3]. [`deep_symbolize_keys`][4] is an alternative to
    `with_indifferent_access`.
    
    [1]: http://api.rubyonrails.org/v5.0.0.1/classes/ActionDispatch/TestResponse.html#method-i-parsed_body
    [2]: http://api.rubyonrails.org/v5.0.0.1/classes/Hash.html#method-i-with_indifferent_access
    [3]: https://www.relishapp.com/rspec/rspec-expectations/v/3-5/docs/composing-matchers#composing-matchers-with-`include`
    [4]: http://api.rubyonrails.org/v5.0.0.1/classes/Hash.html#method-i-deep_symbolize_keys

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -29,20 +29,29 @@ describe 'Sessions resource' do
     it 'fails with email not registered' do
       post sessions_path, params: {email: 'not-registered@example.com'}
       expect(response).to be_not_found
-      expect(response.body).to include('email', 'not-registered@example.com')
+      expect(response.parsed_body.with_indifferent_access).to include(
+        error: a_hash_including(
+          email: a_string_including('not-registered@example.com')
+        )
+      )
     end
 
     it 'fails with wrong password' do
       post sessions_path, params: {email: user.email, password: 'wrong password'}
       expect(response).to be_unprocessable
+      expect(response.parsed_body.with_indifferent_access).to include(
+        error: a_hash_including(
+          password: a_kind_of(String)
+        )
+      )
       expect(response.body).to include('password')
     end
 
     it 'returns usable token' do
       post sessions_path, params: {email: user.email, password: 'correct password'}
       expect(response).to be_ok
-      expect(response.body).to include('token')
-      token = JSON.parse(response.body)['token']
+      expect(response.parsed_body.with_indifferent_access).to include(:token)
+      token = response.parsed_body['token']
       get sessions_path, headers: {authorization: "Token #{token}"}
       expect(response).to be_ok
     end
