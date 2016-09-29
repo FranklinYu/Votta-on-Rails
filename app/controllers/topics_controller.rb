694220bb8bed2fbf788221494841439db4510492
Franklin Yu <franklinyu@hotmail.com>
2016-09-29 19:44:35 -0400

    Generate the Topic scaffold
    Generated with
    
        bin/rails generate scaffold topic \
            title:string body:text user:references \
            --no-controller-specs --no-routing-specs
    
    and migrated with
    
        bin/rake db:migrate

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,53 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :update, :destroy]
+
+  # GET /topics
+  # GET /topics.json
+  def index
+    @topics = Topic.all
+  end
+
+  # GET /topics/1
+  # GET /topics/1.json
+  def show
+  end
+
+  # POST /topics
+  # POST /topics.json
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      render :show, status: :created, location: @topic
+    else
+      render json: @topic.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /topics/1
+  # PATCH/PUT /topics/1.json
+  def update
+    if @topic.update(topic_params)
+      render :show, status: :ok, location: @topic
+    else
+      render json: @topic.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /topics/1
+  # DELETE /topics/1.json
+  def destroy
+    @topic.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def topic_params
+      params.require(:topic).permit(:title, :body, :user_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929210148) do
+ActiveRecord::Schema.define(version: 20160929211940) do
 
   create_table "sessions", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20160929210148) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
