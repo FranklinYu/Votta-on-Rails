5b29ce402627e7e49ea0592f4a41e33a623e7547
Franklin Yu <franklinyu@hotmail.com>
2016-09-30 02:40:48 -0400

    Use private as method modifier

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -42,14 +42,13 @@ class TopicsController < ApplicationController
     @topic.destroy
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_topic
-      @topic = Topic.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  private def set_topic
+    @topic = Topic.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def topic_params
-      params.require(:topic).permit(:title, :body, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  private def topic_params
+    params.require(:topic).permit(:title, :body, :user_id)
+  end
 end
