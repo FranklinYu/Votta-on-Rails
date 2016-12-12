14dd588671ef1e9a20c5e1dda7448450e88c063e
Franklin Yu <franklinyu@hotmail.com>
2016-12-12 02:46:48 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jack = User.create!(email: 'jack@example.com', password: '12341234')
+tom = User.create!(email: 'tom@example.net', password: '12341234')
+
+jack.sessions.create!(comment: 'Safari 10.0.1 on Macintosh 10.11.6')
+jack.sessions.create!(comment: 'curl/7.43.0')
+tom.sessions.create!(comment: 'Chrome 45.0.2454.104 on Macintosh 10.11.6')
+tom.sessions.create!(comment: 'my Ruby bot')
+
+jack.topics.create!(
+  title: 'What is your favorite salad dressing?',
+  body: "There are many, but some of them are really classic and exellent. Dressings with different ingredients are regarded as different ones; ones with same ingredient list, but different amount, are regarded as the same ones."
+)
+jack.topics.create!(
+  title: 'Your favorite text editor software?',
+  body: 'Text editor is essential to programmers!'
+)
