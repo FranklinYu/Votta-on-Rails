8595c20b1489e9dba4bf5230c4d2434dd1d06889
Franklin Yu <franklinyu@hotmail.com>
2016-12-12 06:23:14 -0500

    Update seeds with candidates

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ jack.sessions.create!(comment: 'curl/7.43.0')
 tom.sessions.create!(comment: 'Chrome 45.0.2454.104 on Macintosh 10.11.6')
 tom.sessions.create!(comment: 'my Ruby bot')
 
-jack.topics.create!(
+t = jack.topics.create!(
   title: 'What is your favorite salad dressing?',
   body: "There are many, but some of them are really classic and exellent. Dressings with different ingredients are regarded as different ones; ones with same ingredient list, but different amount, are regarded as the same ones."
 )
@@ -23,3 +23,7 @@ jack.topics.create!(
   title: 'Your favorite text editor software?',
   body: 'Text editor is essential to programmers!'
 )
+
+t.candidates.create!(body: 'Ranch, no doubt!', user: jack)
+t.candidates.create!(body: 'Long live Balsamic!', user: tom)
+
