2be06097c9012c80d97fadf1d1676ac5bb20bcc9
Franklin Yu <franklinyu@hotmail.com>
2017-03-27 16:46:41 -0400

    Fix error
    Fix error introduced by 7ff2a481c726cc23b0a8beeaecf2cd2edac69cbf.
    `Rails` is not available until loaded.

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+# Load the Rails application.
+require_relative 'application'
+
 # this is required by `./secrets.yml`
 if Rails.application.secrets.secret_key_base.nil?
   raise 'secret key base not found; specify it in `config/secrets.yml`'
 end
-
-# Load the Rails application.
-require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
