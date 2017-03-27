7ff2a481c726cc23b0a8beeaecf2cd2edac69cbf
Franklin Yu <franklinyu@hotmail.com>
2017-03-27 03:02:45 -0400

    Test existence of secret key base with Rails
    The environment variable is empty in development and test environment,
    breaking local tests. Testing it with Rails secret is more robust than
    testing the environment variable directly. This allows user to set
    secret in `secret.yml` with another environment variable, or even
    without environment variable, decoupling access of secret key base from
    concrete settings.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 # this is required by `./secrets.yml`
-raise 'secret key base not found' unless ENV['SECRET_KEY_BASE']
+if Rails.application.secrets.secret_key_base.nil?
+  raise 'secret key base not found; specify it in `config/secrets.yml`'
+end
 
 # Load the Rails application.
 require_relative 'application'
